666fb82441b187b8dd02d067c57fc29fcadc19fa
Habi12345 <131537314+Habi12345@users.noreply.github.com>
2024-01-05 11:02:17 +0100

    Day One Done

---
+++ Day1/datatypes.js
@@ -0,0 +1,35 @@
+console.log(typeof 'Crayon');
+console.log('Crayon' instanceof String); // false, it is not an instance of
+console.log(typeof 5);
+// typeof returns the type of a variable and if that variable is an object or not
+console.log(typeof undefined);
+// undefined is a keyword in JavaScript which represents the value of uninitialized variables
+console.log(typeof null);
+// null is also a reserved keyword in JS representing the absence of any value (like NULL in other
+console.log(typeof true);
+// boolean data types represent one bit of information - either true or false
+
+//declaring variables
+let varOne;
+let varTwo;
+let varThree;
+let varFour;
+
+//declaring variables and assigning values
+let that = 'this';
+
+//Declaring my variable and assigning values in multiple line
+let firstName = 'Habib'
+let lastName = 'Abdulkareem'
+let maritalStatus = 'Single'
+console.log(firstName, lastName, maritalStatus)
+
+//Declaring my variable and assigning values in one line
+let fullName = 'Habib Abdulkareem', Sex = 'Male';
+console.log(fullname, Sex)
+
+//Declaration
+let myAge = '19'
+let yourAge = '23'
+
+console.log('I am' + myAge + 'and you are' + yourAge);
---
+++ Day1/variable.js
@@ -0,0 +1,18 @@
+//Comment can make code readable
+//Welcome to 30DaysofJavascript
+/* comments can make code readable, 
+easy to reuse and informative*/
+
+//Assigned String
+let thisVar = 'thatVar';
+
+//Bolean
+true;
+false;
+
+//Undefined
+let firstName;
+console.log(firstName)
+
+//Null
+let emptyValue = null
